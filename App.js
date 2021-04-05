00c5e245f578ba94c695376f2a2289f51f4c4266
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-04-05 11:01:51 +0200

    add router;

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
+
+import AccountDetails from './components/Screens/AccountDetails';
+import AccountEdit from './components/Screens/AccountEdit';
+import Home from './components/Screens/Home';
+import ModalConfirmDelete from './components/Screens/ModalConfirmDelete';
+import StoreAccounts from './components/Screens/StoreAccounts';
+
+const MainStack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator();
+
+const MainStackRouting = () => {
+  return (
+    <MainStack.Navigator initialRouteName="Home">
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen name="AccountEdit" component={AccountEdit} />
+      <MainStack.Screen name="StoreAccounts" component={StoreAccounts} />
+      <MainStack.Screen name="AccountDetails" component={AccountDetails} />
+    </MainStack.Navigator>
+  );
+};
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
+    <RootStack.Navigator mode="modal">
+      <RootStack.Screen name="Main" component={MainStackRouting} />
+
+      <RootStack.Screen
+        name="ModalConfirmDelete"
+        component={ModalConfirmDelete}
+      />
+    </RootStack.Navigator>
   );
-}
+};
 
 export default App;
