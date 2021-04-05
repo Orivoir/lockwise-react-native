b9834df72926fde6bd61b583fb3c34f9be0a818b
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-04-05 16:51:58 +0200

    implements fixtures into App.js

--- App.js
+++ App.js
@@ -1,39 +1,67 @@
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
 
-import {getAll} from './apis/local/accounts';
+import {getAll, clear} from './apis/local/accounts';
 import Root from './components/Root';
 import {Text} from 'react-native';
 
+import {Provider as PaperProvider} from 'react-native-paper';
+import loadFixtures from './load-fixtures';
+
 const App = ({
-  accounts
+  accounts,
+  theme
 }) => {
 
   const dispatch = useDispatch();
 
+  // hydrate accounts
   React.useEffect(() => {
-    getAll()
-    .then(accountsStorage => {
-      dispatch({
-        type: "HYDRATE_ACCOUNTS",
-        accounts: accountsStorage
+    if(accounts instanceof Array) {
+      // accounts has already hydrate
+      return;
+    }
+
+    clear()
+    .then(() => {
+      console.log("storage clear\nstart load fixtures:");
+
+      loadFixtures()
+      .then(accountsLoad => {
+        console.log("append: ",  accountsLoad.length, " accounts fixtures");
+        dispatch({
+          type: "HYDRATE_ACCOUNTS",
+          accounts: accountsLoad
+        });
+
+        getAll()
+        .then(accountsStorage => {
+          console.log("total accounts storage: ", accountsStorage.length);
+        });
+      })
+      .catch(error => {
+        console.error(`load accounts fixture has fail with: ${error.message}`);
+        throw new Error('loads account fixtures has fail');
       });
-    })
-    .catch(error => {
-      console.error(`read accounts error: ${error.message}`);
-      throw new Error('read accounts has crash');
-    })
+
+    });
+
   });
 
   return (
     <>
     {!accounts ? (
       <Text>Loading...</Text>
-    ): <Root />}
+    ): (
+      <PaperProvider theme={theme.value}>
+        <Root />
+      </PaperProvider>
+    )}
     </>
   );
 }
 
 export default connect(state => ({
-  accounts: state.accounts
+  accounts: state.accounts,
+  theme: state.theme
 }))(App);
