e7234dd3c5cb0407ee787363135492402840655b
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-04-06 15:18:43 +0200

    add position dependencies landscape mode to FabCreate component

--- components/FabCreate.js
+++ components/FabCreate.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
 import {FAB} from 'react-native-paper';
 
 const FabCreate = ({onCreate}) => {
+
+  const {width, height} = useWindowDimensions();
+
+  const isLandscape = width >= height;
+
+  const stylesPosition = !isLandscape ? {
+    left: 8,
+    bottom: 8
+  }: {
+    right: 8,
+    top: 8
+  };
+
   return (
     <FAB
       style={{
         position: "absolute",
-        left: 8,
-        bottom: 8
+        ...stylesPosition
       }}
       small={false}
       icon="pen-plus"
