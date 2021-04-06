eb5581e0ee40ed7cef4bd515c8553b5eaf6fb155
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-04-06 13:58:30 +0200

    add action open link login URL

--- components/AccountItem.js
+++ components/AccountItem.js
@@ -4,7 +4,10 @@ import {
   View,
   Text,
   Button,
-  Switch
+  Switch,
+
+  // open link component
+  Linking
 } from 'react-native';
 
 import Clipboard from '@react-native-clipboard/clipboard';
@@ -42,6 +45,29 @@ const AccountItem = ({
     onUpdate(account);
   };
 
+  const onOpenLink = () => {
+    Linking.canOpenURL(account.loginUrl)
+    .then(canOpen => {
+
+      if(canOpen) {
+        Linking.openURL(account.loginUrl)
+        .then(result => {
+          console.log("open url task finish with status: ", result);
+        })
+        .catch(error => {
+          console.error(`open url has crash with: ${error.message}.`);
+          throw new Error('open url has crash.');
+        });
+      } else {
+        console.warn('can open url for:', account.loginUrl);
+      }
+    })
+    .catch(error => {
+      console.error(`check can open url has crash with: ${error.message}`);
+      throw new Error('check can open URL has crash.');
+    });
+  };
+
   return (
     <View>
       <Text>{account.platform}</Text>
@@ -52,7 +78,7 @@ const AccountItem = ({
         <Button title="copy password" onPress={onCopyPassword} />
 
         {!!account.loginUrl && (
-          <Button title="open link" />
+          <Button title="open link" onPress={onOpenLink} />
         )}
 
         <Button title="update" onPress={onLocalUpdate} />
