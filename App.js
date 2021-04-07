3f9a317c9f9b762e15e11cb6ec363b974e38d233
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-04-07 10:33:39 +0200

    implements network state listener into App.js

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import {connect, useDispatch} from 'react-redux';
 import {clear, getAll} from './apis/local/accounts';
 import Root from './components/Root';
 import {Text} from 'react-native';
+import NetworkInfo from '@react-native-community/netinfo';
 
 import {Provider as PaperProvider} from 'react-native-paper';
 import loadFixtures from './load-fixtures';
 
-const App = ({accounts, theme}) => {
+const App = ({accounts, theme, network}) => {
   const dispatch = useDispatch();
 
   // hydrate accounts
@@ -74,6 +75,27 @@ const App = ({accounts, theme}) => {
       });
   });
 
+  React.useEffect(() => {
+    const removeEventListener = NetworkInfo.addEventListener(newNetworkState => {
+      if(
+        newNetworkState.isConnected != network.isConnected ||
+        newNetworkState.isInternetReachable != network.isInternetReachable ||
+        newNetworkState.isWifiEnabled != network.isWifiEnabled ||
+        newNetworkState.details?.ssid != network.details?.ssid
+      ) {
+        console.log("has upgrade network state");
+        dispatch({
+          type: "NETWORK_CHANGE",
+          network: newNetworkState
+        });
+        }
+    });
+
+    return () => {
+      removeEventListener();
+    };
+  });
+
   return (
     <>
       {!accounts ? (
@@ -90,4 +112,5 @@ const App = ({accounts, theme}) => {
 export default connect(state => ({
   accounts: state.accounts,
   theme: state.theme,
+  network: state.network
 }))(App);
