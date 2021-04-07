a9306f7251e38c9e89cab471b52a56622d590c44
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-04-07 10:55:36 +0200

    add floating action button synchronize

---
+++ components/FabSynchronize.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {useWindowDimensions} from 'react-native';
+import {FAB, useTheme} from 'react-native-paper';
+import {useIsFocused} from '@react-navigation/native';
+import { connect } from 'react-redux';
+import {isAvailable} from './../apis/synchronize/accounts';
+
+import {
+  SAFE_SSID
+} from './../constants';
+
+const FabSynchronize = ({network}) => {
+
+  const [isAvailableServerSync, setIsAvailableServerSync] = React.useState(false);
+
+  const {width, height} = useWindowDimensions();
+  const isAvailableNetwork = (network.isConnected && network.isInternetReachable && network.isWifiEnabled && network.details.ssid === SAFE_SSID);
+
+  if(isAvailableNetwork) {
+    isAvailable()
+    .then(isSuccess => {
+      setIsAvailableServerSync(isSuccess);
+    })
+    .catch(error => {
+      console.error(`check status server sync has crash with: ${error.message}`);
+      throw new Error('check status server sync has crash');
+    });
+  }
+
+  const isLandscape = width >= height;
+
+  const {colors} = useTheme();
+  const iconName = isAvailableServerSync ? "database-sync": "sync-alert";
+  const iconColor = isAvailableServerSync ? colors.accent: colors.error;
+
+  const stylesPosition = !isLandscape
+    ? {
+        right: 72,
+        bottom: 48,
+      }
+    : {
+        right: 72,
+        top: 16,
+      };
+
+  return (
+    <FAB
+      style={{
+        position: 'absolute',
+        ...stylesPosition,
+        backgroundColor: iconColor
+      }}
+      small={false}
+      icon={iconName}
+      // onPress={onCreate}
+    />
+  );
+};
+
+export default connect(state => ({
+  network: state.network
+}))(FabSynchronize);
